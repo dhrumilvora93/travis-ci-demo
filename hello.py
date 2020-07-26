76b26c64610ec7149b5c6af88b1020470cf3230c
Dhrumil Vora <dhrumilvora@Dhrumils-MBP.fios-router.home>
2020-07-26 17:09:14 -0400

    added new feature

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
 def add(a:int,b:int):
     return a+b
 
+def subtract(a:int,b:int):
+    return a-b
+
 if __name__=='__main__':
     print(add(2,5))
+    print(subtract(2,5))
--- test_hello.py
+++ test_hello.py
@@ -2,3 +2,7 @@ from hello import *
 
 def test_add():
     assert add(5,2) == 7
+
+
+def test_subtract():
+    assert subtract(5,2) == 3
